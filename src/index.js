e75ff8400e1441e1b37b7861026c9daef8b56138
James <jmsn311@gmail.com>
2024-06-12 09:54:17 -0600

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 // index.js
 
+//Follow link below shows ramens object in array without key of ramens. Go to db.json file and correctly has key?
+fetch('http://localhost:3000/ramens')
+  .then(r => r.json())
+  .then(ramens=> {
+    ramens.forEach((ramen)=> 
+    displayRamens(ramen))
+
+  })
+  
+
 // Callbacks
-const handleClick = (ramen) => {
+
+const handleClick = (ramens) => {
   // Add code
+  e.preventDefault()
+  document.getElementById('h2.name').textContent
+  document.getElementById('h3.restaurant').textContent
+
+
+
 };
 
-const addSubmitListener = () => {
+const addSubmitListener = ("submit",(e) => {
   // Add code
-}
+  document.getElementById('h4')
+ 
+
+  e.preventDefault()
+})
 
 const displayRamens = () => {
   // Add code
-};
-
-const main = () => {
-  // Invoke displayRamens here
+  const ramenImage = document.createElement('img')
+  ramenImage.src = indivBowl.img_url
+  const navbar = document.querySelector("#ramen-menu")
+  
+  };
+  
+  const main = () => {
+    // Invoke displayRamens here
+    navbar.append(ramenImage)
   // Invoke addSubmitListener here
 }
+
+
 
 main()
 
 // Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+// export {
+//   displayRamens,
+//   addSubmitListener,
+//   handleClick,
+//   main,
+// }
